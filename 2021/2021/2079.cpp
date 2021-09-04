164dba4539476ab828d8fbb84d94f0165ab17cc8
pthdud1123 <pthdud1123@naver.com>
2021-09-04 22:52:25 +0900

    20210904_2079:시간초과

--- 2021/2021/2079.cpp
+++ 2021/2021/2079.cpp
@@ -5,7 +5,8 @@
 #include<algorithm>
 using namespace std;
 
-int pelin_check[2002][2002] = { 1, }, pelin_count[2002], pelin_index[2002][2002];//자기자신 
+int pelin_check[2002][2002] = { 1, }, pelin_count[2002];
+bool pelin_index[2002][2002];//자기자신 
 
 
 bool check(string pelindrome, int start, int end) {
@@ -20,12 +21,12 @@ bool check(string pelindrome, int start, int end) {
 	return true;
 }
 
-int pelindrome_count(string pelindrome, int start,int end, int min) {
+int pelindrome_count(string pelindrome, int start, int end, int min) {
 
 	for (int j = 0;j < pelin_count[start];j++) {
 
-		int count = 1,next;
-		if (pelin_index[start][j] <= end) {
+		int count = 1, next;
+		if (j <= end) {
 			next = pelin_index[start][j] + 1;
 		}
 		else {
@@ -39,8 +40,13 @@ int pelindrome_count(string pelindrome, int start,int end, int min) {
 				count++;
 				break;
 			}
-			count++;
-			next = pelin_index[next][0] + 1;
+			for (int k = 0;k < pelin_count[next];k++) {
+				if (pelin_index[next][k] <= end) {
+					count++;
+					next = pelin_index[next][k] + 1;
+					break;
+				}
+			}
 		}
 		if (min > count) {
 			min = count;
@@ -51,35 +57,32 @@ int pelindrome_count(string pelindrome, int start,int end, int min) {
 
 int main() {
 
-
+	ios::sync_with_stdio(false);
 	string pelindrome;
 
 	cin >> pelindrome;
 
 
 	int size = pelindrome.size() - 1;
-	
+
 	int result = 0;//결과 값
 
 	//각 원소의 위치에서 pelindrome이 가능한 원소 구하기
 	for (int i = 0;i <= size;i++) {
 		pelin_count[i] = 1;
-		int index = 0;
 		for (int j = size;j > i;j--) {
 			if (pelindrome[i] == pelindrome[j]) {
 				bool key = check(pelindrome, i, j);
 				if (key) {
 					pelin_count[i]++;
-					pelin_index[i][index] = j;
-					index++;
+					pelin_index[i][j] = true;
 				}
 			}
 
 		}
-		pelin_index[i][index] = i;
 	}
 
-	int min = 3000, count=0;
+	int min = 3000, count = 0;
 
 	for (int i = 0;i <= size;i++) {
 		if (i != 0) {
@@ -87,17 +90,17 @@ int main() {
 			int tmp_min = 3000;
 			for (int ii = 0;ii < i;ii++) {
 				count = ii;
-				count += pelindrome_count(pelindrome, 0,i-1, tmp_min);
+				count += pelindrome_count(pelindrome, ii, i - 1, tmp_min);
 				if (tmp_min > count)
 					tmp_min = count;
 			}
 			count = tmp_min;
 		}
-		count+=pelindrome_count(pelindrome, i,size, 3000);
+		count += pelindrome_count(pelindrome, i, size, 3000);
 		if (min > count) {
 			min = count;
 		}
-		
+
 	}
 	cout << min;
 
