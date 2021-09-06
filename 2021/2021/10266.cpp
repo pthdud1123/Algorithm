f947275ff828b0c4016bc6305e548e13a689f333
pthdud1123 <pthdud1123@naver.com>
2021-09-06 22:22:06 +0900

    20210906_10266

--- 2021/2021/10266.cpp
+++ 2021/2021/10266.cpp
@@ -2,38 +2,27 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+#define MAX 360000
 
 
 int main() {
-	vector<vector<int>> watch;
-	vector<vector<int>> angle;
+	vector<vector<int>> watch(2,vector<int>(MAX,0));
 	int n;
 	cin >> n;
 	for (int q = 0;q < 2;q++) {
-		vector<int> tempwatch;
 		for (int i = 0;i < n;i++) {
-			int temp;
-			cin >> temp;
-			tempwatch.push_back(temp);
+			int index;
+			cin >> index;
+			watch[q][index] = 1;
 		}
-		sort(tempwatch.begin(), tempwatch.end());
-		watch.push_back(tempwatch);
 	}
-	//각 근접해있는 시계바늘 사이간의 angle구하기
-	for (int i = 0;i < 2;i++) {
-		vector<int> tempangle;
-		for (int j = 0;j < n-1;j++) {
-			int a;
-			a = watch[i][j + 1] - watch[i][j];
-			if (a >= 180000) {
-				a = 360000 - a;
-			}
-			tempangle.push_back(a);
-		}
-		angle.push_back(tempangle);
-	}
+	//watch[2]접두사 접미사 체크
 
-	if (angle[0]==angle[1]) {
+
+	//KMP알고리즘
+	
+
+	if (watch[0]== watch[1]) {
 		cout << "possible";
 	}
 	else {
