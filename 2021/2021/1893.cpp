3d7740e63d66c04dbb475af54c40a517e18dbd8e
pthdud1123 <pthdud1123@naver.com>
2021-09-10 17:30:47 +0900

    20210910_1893

--- 2021/2021/1893.cpp
+++ 2021/2021/1893.cpp
@@ -5,59 +5,64 @@
 #include<algorithm>
 using namespace std;
 
-vector<int> RabinKarp(string S, string W,string A) {
-	vector<int> shift;//반환할 쉬프트 값
-	int Ssize = S.size(), Wsize = W.size();
-	int whash=0,shash=0;
-	//W해시값 구하기
-	for (int i = 0,j=0;i < Wsize;i++) {
-		whash = whash + A[W[Wsize - 1 - i]-1] * pow(2, j);
-		j++;
-	}
-	//각 해시 값 구해서 W해시 값과 비교하기
-	for (int k = 1;k < Wsize;k++) {//k는 이동한 쉬프트에서 원문값이 있는 지 체크
-		shash = 0;
-		for (int i = 0;i < Ssize;i++) {
-			S[i]++;
-			if (S[i] > Wsize)
-				S[i] = 1;
+vector<int> table(string W) {
+	vector<int> t(W.size() + 1, 0);
+	int j = 0;
+	for (int i = 1;i < W.size();i++) {
+		while (j > 0 && W[i] != W[j]) {
+			j = t[i - 1];
 		}
-
-		for (int i = 0;i < Ssize-Wsize;i++) {
-			if (i == 0) {
-				for (int j = 0;j < Wsize;j++) {
-					shash = shash + A[S[Wsize - j - 1] - 1] * pow(2, j);
-				}
+		if (W[i] == W[j]) {
+			t[i] = ++j;
+		}
+	}
+	return t;
+}
+bool KMP(string p, string t) {
+	vector<int> checktable = table(t);
+	int j = 0;
+	for (int i = 0;i < p.size() - t.size();i++) {
+		while (j > 0 && p[i] != t[j]) {
+			j = checktable[j - 1];
+		}
+		if (p[i] == t[j]) {
+			if (j == t.size() - 1) {
+				return true;
 			}
 			else {
-				//다음값 =2*(이전 해시 값 - 이전 문자열 맨 앞자리(a))-현재 추가 되는 뒷자리 문자(b).
-				int a = A[S[i - 1]-1] * pow(2, Wsize - 1);
-				int b = A[S[i + Wsize - 1]-1] * pow(2, 0);
-				shash = 2 * (shash - a) + b;
+				j++;
 			}
-			if (shash == whash) {
-				//해쉬값이 맞다면 문자 값도 맞는지 확인해주기
-				bool key = true;
-				for (int x = i,y=0;x < Wsize;x++,y++) {
-					if (S[x] != W[y]) {
-						key = false;
-						break;
-					}
-				}
-				if (key) {
-					shift.push_back(k);
-					break;
-				}
-			}
-
 		}
+	}
+	return false;
+
+}
+
+vector<int> RabinKarp(string S, string W, string A) {
+	vector<int> shift;//반환할 쉬프트 값
+	int Ssize = S.size(), Wsize = W.size();
+
+	//각 해시 값 구해서 W해시 값과 비교하기
+	for (int k = 1;k < Wsize;k++) {//k는 이동한 쉬프트에서 원문값이 있는 지 체크
+
+		for (int i = 0;i < Wsize;i++) {
+			W[i]++;
+			if (W[i] > Wsize)
+				W[i] = 1;
+		}
+
+		bool key = KMP(S, W);
+		if (key) {
+			shift.push_back(k);
+		}
+
 	}
 	return shift;
 
 }
 
 int main() {
-	int T,t=0;
+	int T, t = 0;
 	cin >> T;
 	while (1) {
 		vector<char>ascii(200, 0);//아스키코드 순서 체크할 배열
@@ -66,7 +71,7 @@ int main() {
 		}
 		t++;
 
-		string a,A, W, S;
+		string A, W, S;
 		cin >> A >> W >> S;
 
 		//아스키코드에 순서 매기기
@@ -81,7 +86,7 @@ int main() {
 		for (int i = 0;i <= S.size();i++) {
 			S[i] = ascii[S[i]];
 		}
-		
+
 		if (S == W) {
 			//암호랑 원문이랑 같은 경우 
 			cout << "unique: 0" << endl;
@@ -90,11 +95,11 @@ int main() {
 			vector<int> shift = RabinKarp(S, W, A);
 			if (shift.empty()) {
 				//비어있을 경우
-				cout << "no solution"<<endl;
+				cout << "no solution" << endl;
 			}
 			else {
 				if (shift.size() == 1) {
-					cout << "unique: " << shift[0]<<endl;
+					cout << "unique: " << shift[0] << endl;
 				}
 				else {
 					cout << "ambiguous: ";
