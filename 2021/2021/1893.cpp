bf4152bf1ee4aa108c64544a1ed9588b44074420
pthdud1123 <pthdud1123@naver.com>
2021-09-11 22:57:34 +0900

    20210911_1893

--- 2021/2021/1893.cpp
+++ 2021/2021/1893.cpp
@@ -1,3 +1,77 @@
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//using namespace std;
+//
+//vector<int> maketable(string t) {
+//	vector<int> table(t.size(),0);
+//	int j = 0;
+//	for (int i = 0;i < t.size();i++) {
+//		while (j > 0 && t[j] != t[i + j]) {
+//			j = table[j - 1];
+//		}
+//		if(t[i]==t[j]) {
+//			table.push_back(++j);
+//		}
+//	}
+//	return table;
+//}
+//
+//vector<int> KMP(int ascii[],string w,string s,string a){
+//	vector<int> shift;
+//	vector<int> t=maketable(w);
+//	for (int k = 1;k < a.size();k++) {
+//		int j = 0;
+//		for (int i = 0;i < s.size()-w.size();i++) {
+//			while (j > 0 && a[ascii[s[i]]] != a[(ascii[w[j]]+k)%a.size()]) {
+//				j = t[j - 1];
+//			}
+//			if (a[ascii[s[i]]] == a[(ascii[w[j]] + k) % a.size()]) {
+//				if (j == w.size() - 1) {
+//					shift.push_back(k);
+//					break;
+//				}
+//				else {
+//					j++;
+//				}
+//			}
+//		}
+//	}
+//	return shift;
+//}
+//
+//int main() {
+//	int t;
+//	cin >> t;
+//	while (t--) {
+//		string A, W, S;
+//		cin >> A >> W >> S;
+//		if (W == S) {
+//			cout << "unique: 0" << endl;
+//			continue;
+//		}
+//		int ascii[300] = {0};
+//		for (int i = 0;i < A.size();i++) {
+//			ascii[A[i]] = i;
+//		}
+//		vector<int> shift=KMP(ascii, W, S,A);
+//		if (shift.empty()) {
+//			cout << "no solution" << endl;
+//		}
+//		else {
+//			if (shift.size() == 1) {
+//				cout << "unique: " << shift[0] << endl;
+//			}
+//			else {
+//				cout << "ambiguous: ";
+//				for (int i = 0;i < shift.size();i++) {
+//					cout << shift[i] << " ";
+//				}
+//				cout << endl;
+//			}
+//		}
+//	}
+//}
 #include<iostream>
 #include<string>
 #include<vector>
@@ -45,10 +119,10 @@ vector<int> RabinKarp(string S, string W, string A) {
 	//각 해시 값 구해서 W해시 값과 비교하기
 	for (int k = 1;k < Wsize;k++) {//k는 이동한 쉬프트에서 원문값이 있는 지 체크
 
-		for (int i = 0;i < Wsize;i++) {
-			W[i]++;
-			if (W[i] > Wsize)
-				W[i] = 1;
+		for (int i = 0;i < Ssize;i++) {
+			S[i]++;
+			if (S[i] > Wsize)
+				S[i] = 1;
 		}
 
 		bool key = KMP(S, W);
