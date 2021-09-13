244432c5e37230032c6228c291ba828e7c787d70
pthdud1123 <pthdud1123@naver.com>
2021-09-13 21:32:23 +0900

    20210913_complete

--- 2021/2021/1893.cpp
+++ 2021/2021/1893.cpp
@@ -1,74 +1,94 @@
 #include<iostream>
 #include<vector>
+#include<string.h>
 #include<string>
+#include<map>
 using namespace std;
 
 vector<int> maketable(string t) {
-	vector<int> table(t.size(),0);
+	vector<int> table(t.size(), 0);
 	int j = 0;
-	for (int i = 0;i < t.size();i++) {
-		while (j > 0 && t[j] != t[i + j]) {
+	for (int i = 1;i < t.size();i++) {
+		while (j > 0 && t[i] != t[j])
 			j = table[j - 1];
-		}
-		if(t[i]==t[j]) {
-			table.push_back(++j);
+		if (t[i] == t[j]) {
+			table[i]=++j;
 		}
 	}
 	return table;
 }
 
-vector<int> KMP(int ascii[],string w,string s,string a){
+vector<int> KMP(string p, string t, string a, map<char, int> o) {
 	vector<int> shift;
-	vector<int> t=maketable(w);
-	for (int k = 0;k < a.size();k++) {
-		int j = 0;
-		int check = 0;
-		for (int i = 0;i < s.size();i++) {
-			while (j > 0 && s[i] != a[(ascii[w[j]]+k)%a.size()]) {
-				j = t[j - 1];
-			}
-			if (s[i] == a[(ascii[w[j]] + k) % a.size()]) {
-				if (j == w.size() - 1) {
-					check++;
-					j = t[j];
+	vector<int> numbert;
+	int shiftsize = a.size();
+	
+	for (int j = 0;j < t.size();j++) {
+		numbert.push_back((o[t[j]]) % shiftsize);//원문 이동
+	}
+	for (int i = 0;i < shiftsize;i++) {
+		if(i!=0) {
+			for (int j = 0;j < t.size();j++) {
+				numbert[j] = (numbert[j] + 1) % shiftsize;
+				if (numbert[j] - 1 == -1) {
+					t[j] = a[a.size()-1];
 				}
 				else {
-					j++;
+					t[j] = a[numbert[j] - 1];
 				}
 			}
 		}
-		if (check == 1) {
-			shift.push_back(k);
+		int index = 0;
+		int cnt = 0;
+		vector<int> table = maketable(t);
+		for (int j = 0;j < p.size();j++) {
+			while (index > 0 && p[j] != t[index])
+				index = table[index - 1];
+			if (p[j] == t[index]) {
+				if (index == t.size() - 1) {
+					cnt++;
+					index = table[index];
+				}
+				else {
+					index++;
+				}
+			}
+		}
+		if (cnt == 1) {
+			shift.push_back(i);
 		}
 	}
 	return shift;
 }
-
-int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		string A, W, S;
-		cin >> A >> W >> S;
-		int ascii[300] = {0};
-		for (int i = 0;i < A.size();i++) {
-			ascii[A[i]] = i;
-		}
-		vector<int> shift=KMP(ascii, W, S,A);
-		if (shift.empty()) {
-			cout << "no solution" << endl;
+void outprint(vector<int>shift) {
+	if (shift.empty()) {
+		cout << "no solution" << endl;
+	}
+	else {
+		if (shift.size() == 1) {
+			cout << "unique: " << shift[0] << endl;
 		}
 		else {
-			if (shift.size() == 1) {
-				cout << "unique: " << shift[0] << endl;
+			cout << "ambiguous:";
+			for (int i = 0;i < shift.size();i++) {
+				cout << " " << shift[i];
 			}
-			else {
-				cout << "ambiguous:";
-				for (int i = 0;i < shift.size();i++) {
-					cout << ' ' << shift[i];
-				}
-				cout << endl;
-			}
+			cout << endl;
 		}
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		string A, W, S;
+		map<char, int> order;
+		cin >> A >> W >> S;
+		for (int i = 0;i < A.size();i++) {
+			order[A[i]] = (i+1)%A.size();
+		}
+		vector<int> shift = KMP(S, W,A, order);
+		outprint(shift);
 	}
 }
